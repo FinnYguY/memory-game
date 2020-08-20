2958cdfc2233ebe4938ab74b5dc13c382c8c8f64
finnyguy <tema.menschikoff2013165@gmail.com>
2020-08-20 15:57:35 +0300

    fix: eslint changes

--- src/view.js
+++ src/view.js
@@ -1,12 +1,11 @@
 const processor = require('.\\processor.js');
 
-//create the HTML
+// create the HTML
 exports.generateView = function () {
-
-  //creating fieldSelector block and its contents
-  let fieldSelectorBlock = processor.createHtml('div', 'field-selector-block');
-  let fieldSelector = processor.createHtml('select', 'field-selector-block__selector');
-  let fieldSelectorButton = processor.createHtml('button', 'button field-selector-block__button', 'Confirm & Shuffle');
+  // creating fieldSelector block and its contents
+  const fieldSelectorBlock = processor.createHtml('div', 'field-selector-block');
+  const fieldSelector = processor.createHtml('select', 'field-selector-block__selector');
+  const fieldSelectorButton = processor.createHtml('button', 'button field-selector-block__button', 'Confirm & Shuffle');
 
   document.body.append(fieldSelectorBlock);
   fieldSelectorBlock.append(processor.createHtml('span', 'span_blurred', 'Select field size'));
@@ -17,8 +16,8 @@ exports.generateView = function () {
   fieldSelectorBlock.append(fieldSelectorButton);
   fieldSelectorButton.addEventListener('click', processor.changeFieldSize);
 
-  //creating timeNturnsBlock with buttons
-  let timeNturnsBlock = processor.createHtml('div', 'time-n-turns-block');
+  // creating timeNturnsBlock with buttons
+  const timeNturnsBlock = processor.createHtml('div', 'time-n-turns-block');
 
   document.body.append(timeNturnsBlock);
   timeNturnsBlock.append(processor.createHtml('span', 'span_blurred', 'Time'));
@@ -26,14 +25,14 @@ exports.generateView = function () {
   timeNturnsBlock.append(processor.createHtml('span', 'span_blurred', 'Turns'));
   timeNturnsBlock.append(processor.createHtml('input', 'time-n-turns-block__button turnsCounter', '', 'value', '0', 'disabled', 'true'));
 
-  //creating gamefield
+  // creating gamefield
   document.body.append(processor.createHtml('div', 'gamefield gamefield_4x4'));
 
-  //creating block with information
+  // creating block with information
   document.body.append(processor.createHtml('div', 'info-block', '<b>Memory Game</b> <br> Flip two cards. If the cards are the same on their face side - they disappear. If they are different - you have to pick another pair. Clearing the field is the winning condition <br> <b>Click</b> on any card to start playing <br> <b>Confirm & Shuffle</b> - shuffle field of selected size'));
 
-  //creating best results block
-  let leaderboardBlock = processor.createHtml('div', 'leaderboard-block', '<b>Leaderboard</b>');
+  // creating best results block
+  const leaderboardBlock = processor.createHtml('div', 'leaderboard-block', '<b>Leaderboard</b>');
 
   document.body.append(leaderboardBlock);
   leaderboardBlock.append(processor.createHtml('div', 'lineBreak'));
@@ -48,7 +47,7 @@ exports.generateView = function () {
   document.querySelector('.size66').before(processor.createHtml('div', 'wrapper__record', '<b>6x6</b>'));
   leaderboardBlock.append(processor.createHtml('div', 'lineBreak'));
 
-  //creating save/load block
+  // creating save/load block
   leaderboardBlock.append(processor.createHtml('div', 'saveLoadBlock', 'Statistics are not recorded for loaded games, train in real time to conquer leaderboard! <br>'));
   document.querySelector('.saveLoadBlock').append(processor.createHtml('button', 'button  saveGameButton', 'Save Game'));
   document.querySelector('.saveGameButton').addEventListener('click', processor.saveGame);
@@ -56,4 +55,4 @@ exports.generateView = function () {
   document.querySelector('.loadGameButton').addEventListener('click', () => {
     processor.loadGame(localStorage.getItem('gameSave'));
   });
-}
+};
