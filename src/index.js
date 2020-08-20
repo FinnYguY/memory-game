0ecf91043da864aa9716a6c22d6dca6a2e290496
finnyguy <tema.menschikoff2013165@gmail.com>
2020-08-20 15:56:54 +0300

    Revert "fix: eslint changes"
    This reverts commit 614a4d13b7387f6304ae736c1a017c2fc06facda.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 const processor = require('.\\processor.js');
-
 const view = require('.\\view.js');
-
 import '.\\scss\\styles.scss';
 
-// all required onload initializations
-function initialize() {
+//all required onload initializations
+function initialize () {
   processor.addCards(16);
   processor.showLeaderboard();
 }
 
-// create the HTML
+//create the HTML
 view.generateView();
 
 window.onload = initialize();
 
-// configuring the mutation observer for delegation to work properly
-const field = document.getElementsByTagName('body')[0];
+//configuring the mutation observer for delegation to work properly
+var field = document.getElementsByTagName('body')[0];
 
 const config = {
-  childList: true,
+    childList: true
 };
 
 const observer = new MutationObserver(processor.callback);
